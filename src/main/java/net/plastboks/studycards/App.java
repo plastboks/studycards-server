2942ca514a36adbcfc1b62f9c6a9535f3c8af204
Alexander Skjolden <alex@plastboks.net>
2015-12-16 14:29:22 +0100

    initial commit
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

---
+++ src/main/java/net/plastboks/studycards/App.java
@@ -0,0 +1,14 @@
+package net.plastboks.studycards;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by alex on 12/16/15.
+ */
+public class App
+{
+    public static void main(String ... args)
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
