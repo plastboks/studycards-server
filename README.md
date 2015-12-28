d2001c9eccb4f1fd127e55222c911b416b2849d8
Alexander Skjolden <alex@plastboks.net>
2015-12-28 17:52:59 +0100

    fixed some bugs for getting jersey up and running
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
+## Maven
+* `mvn clean test`
+* `mvn exec:java`
+
 ## Resources
 
 * https://spring.io/guides/tutorials/bookmarks/
 * http://docs.spring.io/spring/docs/3.0.0.M3/reference/html/ch18s02.html
 * http://docs.spring.io/spring-boot/docs/current/reference/html/common-application-properties.html
+* https://jersey.java.net/documentation/latest/getting-started.html
--- src/main/java/net/plastboks/studycards/App.java
+++ src/main/java/net/plastboks/studycards/App.java
@@ -1,11 +1,31 @@
 package net.plastboks.studycards;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
 /**
  * Created by alex on 12/16/15.
  */
 public class App
 {
-    public static void main(String ... args)
+    public static final String BASE_URI = "http://localhost:8080/myapp/";
+
+    public static HttpServer startServer()
     {
+        final ResourceConfig rc = new ResourceConfig().packages("net.plastboks.studycards");
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String ... args) throws IOException
+    {
+        final HttpServer server = startServer();
+        System.out.println(String.format("Jersey app started with WADL available at "
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+        System.in.read();
+        server.stop();
     }
 }
