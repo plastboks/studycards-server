4cbd23d7db7bf5e80ae01ab5660e8681f4031736
Alexander Skjolden <alex@plastboks.net>
2016-01-12 12:16:15 +0100

    added sqlite for as an postgresql alternative - missing config file
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- src/main/java/net/plastboks/studycards/App.java
+++ src/main/java/net/plastboks/studycards/App.java
@@ -2,6 +2,7 @@ package net.plastboks.studycards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Created by alex on 12/16/15.
@@ -11,6 +12,10 @@ public class App
 {
     public static void main(String ... args)
     {
+        //AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        //ctx.register(AppConfig.class);
+        //ctx.refresh();
+
         SpringApplication.run(App.class, args);
     }
 }
